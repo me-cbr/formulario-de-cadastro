b09bd4acc4cc988ead98cb3514c32849211c4624
me-cbr <mecoelhobrocha@outlook.com>
2024-05-03 17:15:33 -0300

    Add my first models

--- register/models.py
+++ register/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Register(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    name = models.TextField(max_length=255)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
