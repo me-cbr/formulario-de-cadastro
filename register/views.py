ac5e323cc1cf8d2c253f9f86a96c703051ca98aa
me-cbr <mecoelhobrocha@outlook.com>
2024-05-03 17:46:30 -0300

    Add new def on views

--- register/views.py
+++ register/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import Register
 
 
 def index(request):
     return render(request, 'pages/index.html')
+
+def list(request):
+    new_user = Register()
+    new_user.request.POST.get('name_user')
+    new_user.request.POST.get('age_user')
+    new_user.save()
+
+    users = {
+        'users': Register.objects.all()
+    }
+
+    return render(request, 'pages/list.html', users)
